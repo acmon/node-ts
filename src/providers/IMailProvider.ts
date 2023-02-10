917aa49cfb4ac5a5e85e61b61cd25ffb985aff1b
acmon <acmon.soares@gmail.com>
2023-02-10 18:35:05 -0300

    feat: first commit

---
+++ src/providers/IMailProvider.ts
@@ -0,0 +1,14 @@
+export interface IAddress {
+  email: string;
+  name: string;
+}
+export interface IMessage {
+  from: IAddress;
+  to: IAddress;
+  subject: string;
+  body: string;
+}
+
+export interface IMailProvider {
+  sendMail(message: IMessage);
+}
